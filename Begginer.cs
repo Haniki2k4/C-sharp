8c555c86484a03495b55d4a06315fed017f2df1a
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2023-12-01 23:17:26 +0700

    Update Begginer.cs

--- Begginer.cs
+++ Begginer.cs
@@ -16,12 +16,55 @@ namespace ConsoleApp1
             Console.Write("Im a new student on C# and my birth is ");
             Console.WriteLine(2004);
             Console.WriteLine("I'm Male");
-            /*Console.ReadKey(true); 
+            /*Console.ReadKey(true); có thể dùng như Readline
              * () mặc định là false
              * (false) hiện thị phím ấn trên màn hình
              * (true) không hiện thị phím ấn
             */
+            
+            int soLuong = 0; //khai bao  bien
+            float diemTB = 8.5f;
+            string name = "Trang";
+            byte tuoi = 24;
+            Console.WriteLine("So Luong la: {0}", soLuong); //truyền giá trị soluong vào {0}
+            Console.WriteLine("Diem trung binh cua {0} la: {1}", name, diemTB); // truyền giá trị name và diemTB lần lượt và {0}, {1}
+
+            /*quy tắc đặt tên (camel):
+             * đặt tên sát với mục đích sử dụng biến
+             * từ chữ cái thứ 2 thì viết hoa chữ cái đầu
+             *          vd: diemTrungbinh
+             *              diemToan
+             */
+
+            int a = 2;
+            int b = 3;
+            float c = a / b;
+            float d = (float)a / b; //ép kiểu
+            Console.WriteLine("gia tri cua c la: {0}", c);
+            Console.WriteLine("gia tri cua d la: " + d);
+
+            string hoTen; //khai báo biến
+            float diemToan;
+            float diemVan;
+
+            Console.WriteLine("Moi nhap vao ho ten: ");
+            hoTen = Console.ReadLine(); //đọc giữ liệu nhập vào
+
+            Console.WriteLine("Moi nhap diem Toan: ");
+            diemToan = float.Parse(Console.ReadLine()); //đọc dữ liệu nhập vào và ép kiểu float
+
+            Console.WriteLine("Moi nhap diem Van: ");
+            diemVan = float.Parse(Console.ReadLine()); //đọc dữ liệu nhập vào và ép kiểu float
+
+            Console.WriteLine("Hoc sinh {0} co diem toan la {1} va diem Van la {2}", hoTen, diemToan, diemVan);
+
+            var y = 241; //ngầm hiểu kiểu dữ liệu (ko nên lạm dụng)
+            //nếu như vdu là 12.4 thì sẽ định dạng là double chứ ko phải float
+            Console.WriteLine("kieu du lieu cua y: " + y.GetType().ToString());
+
+            //Console.ReadKey()
             Console.ReadLine(); //dừng chương trình là và giữ không auto off chương trình
+
         }
     }
 }
