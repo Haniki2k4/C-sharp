6d8aa0ed850ab50864b6a891e2daee59b0dd62d6
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2023-12-06 13:55:07 +0700

    Update Exercise For Loop.cs

--- Exercise For Loop.cs
+++ Exercise For Loop.cs
@@ -111,7 +111,43 @@ namespace Beginner
             {
                 Console.WriteLine(x);
             }
-                Console.ReadKey();
+
+            /*int N = 10;
+            int M = 20;
+
+            char drawChar = '*';
+            char insideChar = ' ';
+
+
+            // Vẽ từ trên xuống
+            for (int i = 0; i < N; i++)
+            {
+                // Vẽ từ trái sang
+                for (int j = 0; j < M; j++)
+                {
+                    /*
+                     * Nếu đang ở tọa độ là cạnh trên hoặc dưới i % (N - 1) == 0
+                     * hoặc đang ở cạnh trái hoặc phải (j % (M - 1) == 0)
+                     * mà không nằm ở cạnh trên hoặc dưới (i % (N - 1) != 0)
+                     * ((i % (N - 1) != 0) && (j % (M - 1) == 0))
+                     * thì vẽ ra ký tự của hình chữ nhật
+                     * ngược lại vẽ ra ký tự không thuộc hình chữ nhật
+                    */
+
+                    if (i % (N - 1) == 0 || ((i % (N - 1) != 0) && (j % (M - 1) == 0)))
+                    {
+                        Console.Write(drawChar);    // lúc này là ký tự *
+                    }
+                    else
+                    {
+                        Console.Write(insideChar);  // lúc này là ký tự rỗng ' '
+                    }
+                }
+                //mỗi lần vẽ xong một hàng thì xuống dòng
+                Console.WriteLine();
+            }
+            Console.ReadKey();*/
+            
         }
     }
 }
