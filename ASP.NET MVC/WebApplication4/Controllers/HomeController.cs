de0fc01cd92a287681f8d624c6428aaba62d334a
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2024-01-09 19:12:18 +0700

    Add files via upload

---
+++ ASP.NET MVC/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //1 hàm -> 1 view
+        //1 controller -> nhiều action
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+
+            //chuyển hướng chủ động trên server qua action khác, cùng controller
+            //return RedirectToAction("Login");
+
+            //chuyển hướng chủ động trên server qua action khác, khác controller
+            //return RedirectToAction("ListNews", "TinTuc");
+
+            //chuyển hướng chủ động trên server qua action khác, khác controller
+            //return RedirectToAction("ListNews", "TinTuc");
+
+            //chuyển hướng theo link
+            //return Redirect("https://google.com");
+
+            //chuyển hướng = route
+            //return RedirectToRoute("Default");
+        }
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        //chuyển hướng trên server
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC/WebApplication4/Controllers/TinTucController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class TinTucController : Controller
+    {
+        // GET: TinTuc
+        public ActionResult ListNews()
+        {
+            return View();
+        }
+        public ActionResult ReadNews()
+        {
+            return View();
+        }
+
+    }
+}
